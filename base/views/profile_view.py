af41ce2ed1b498d4b64e34abf029743e6f7f5c20
shima <ajdp25137@gmail.com>
2026-04-14 23:29:49 +0900

    修正

--- base/views/profile_view.py
+++ base/views/profile_view.py
@@ -8,7 +8,7 @@ User = get_user_model()
 
 class ProfileDetailView(LoginRequiredMixin, DetailView):
     model = User
-    template_name = 'auth/profile_detail.html'
+    template_name = 'profile/detail.html'
     context_object_name = 'user'
 
     def get_object(self):
@@ -17,13 +17,13 @@ class ProfileDetailView(LoginRequiredMixin, DetailView):
 
 class ProfileEditView(LoginRequiredMixin, UpdateView):
     model = User
-    template_name = 'auth/profile_edit.html'
+    template_name = 'profile/edit.html'
     # CustomUserモデルで定義したフィールド名を指定
-    fields = ('username', 'email', 'zip_code', 'address', 'phone_number')
+    fields = ('name', 'email', 'postal_code', 'address', 'phone_number')
 
     def get_success_url(self):
         # 保存完了後のリダイレクト先（urls.pyのname='top_page'を想定）
-        return reverse_lazy('top_page')
+        return reverse_lazy('profile_detail')
 
     def get_object(self):
         return self.request.user
